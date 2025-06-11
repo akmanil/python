23ba4595876f7a4b128a56572660d741fd21a1b2
akmanil <maharanaanil424@gmail.com>
2025-06-11 14:37:37 +0530

    Adding the datatypes

---
+++ datatypes.py
@@ -0,0 +1,12 @@
+# no need to state the type of data
+
+number = 10;
+str = "Hello";
+boolean = True ;
+array =[1, 2 ,3];
+tupple =(1,2,3);
+dictionary = {"name":"Anil","age":23};
+# this all aree the the data types in python
+
+# to print that all the variables or other in console we just to use print statemate in python
+print(number , str ,boolean , array ,tupple ,dictionary);
